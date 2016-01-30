1cc8b5cb1757fad2d05b65d5b2fedbd33f42b38d
Joseph Marshall <marsha87@purdue.edu>
2016-01-30 02:58:29 -0500

    Added Title

---
+++ Assets/PencilSharpening/OutOfBounds.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OutOfBounds : MonoBehaviour {
+
+	private float timer;
+
+	void Awake() {
+		timer = Time.time;
+	}
+
+	void LateUpdate() {
+		if (timer < Time.time - 2) {
+			if (GameObject.FindGameObjectWithTag("Pencil") == null) {
+				Debug.Log("YOU LOSE!");
+			}
+			timer = Time.time;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.collider.gameObject.tag.Equals("Pencil")) {
+			Destroy(other.collider.gameObject);
+		} else {
+			Destroy(other.collider.gameObject);
+		}
+	}
+}
