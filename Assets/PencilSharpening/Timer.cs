a49fdbe35c33497ac226f127788fcfdae5531302
Joseph Marshall <marsha87@purdue.edu>
2016-01-30 16:50:43 -0500

    Added Timer Prefab
    Timer Prefrab

---
+++ Assets/PencilSharpening/Timer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	public Sprite[] sprites = new Sprite[11];
+
+	SpriteRenderer timerSprite;
+
+	float timer = 0f;
+	int clock = 11;
+
+	// Use this for initialization
+	void Awake () {
+		timerSprite = GetComponent<SpriteRenderer>();
+		timer = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer < Time.time - 1f) {
+			clock = Mathf.Clamp(clock - 1, 0, 10);
+			timerSprite.sprite = sprites[clock];
+			timer = Time.time;
+		}
+	}
+}
