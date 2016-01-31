18936723d27862122a1363bddbd58b65ec41b340
Joseph Marshall <marsha87@purdue.edu>
2016-01-30 22:09:17 -0500

    UpdatedScene and Timer

--- Assets/PencilSharpening/Timer.cs
+++ Assets/PencilSharpening/Timer.cs
@@ -10,6 +10,9 @@ public class Timer : MonoBehaviour {
 	float timer = 0f;
 	int clock = 11;
 
+	float nextScene = 0f;
+	bool next = false;
+
 	// Use this for initialization
 	void Awake () {
 		timerSprite = GetComponent<SpriteRenderer>();
@@ -22,6 +25,22 @@ public class Timer : MonoBehaviour {
 			clock = Mathf.Clamp(clock - 1, 0, 10);
 			timerSprite.sprite = sprites[clock];
 			timer = Time.time;
+			if (clock == 0) {
+				StartTimer();
+			}
+		}
+
+		if (next) {
+			if (nextScene < Time.time - 3/*seconds*/) {
+				GameController.control.NextScene();
+			}
+		}
+	}
+
+	void StartTimer() {
+		if (!next) {
+			nextScene = Time.time;
+			next = true;
 		}
 	}
 }
